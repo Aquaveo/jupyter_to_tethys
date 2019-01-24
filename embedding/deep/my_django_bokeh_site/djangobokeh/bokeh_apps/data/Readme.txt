4c6059592ba614d9f517083463ff7d183ec61a1f
Zhiyu Li <zli@aquaveo.local>
2019-01-24 16:23:10 -0700

    update notes

--- embedding/deep/my_django_bokeh_site/djangobokeh/bokeh_apps/data/Readme.txt
+++ embedding/deep/my_django_bokeh_site/djangobokeh/bokeh_apps/data/Readme.txt
@@ -1,2 +1,3 @@
-Download sample data for nyc_pickup.py at https://s3.amazonaws.com/datashader-data/nyc_taxi_wide.parq
-And put in this folder "jupyter_to_tethys/embedding/deep/my_django_bokeh_site/djangobokeh/bokeh_apps/data"
+nyc_pickup.py
+Download sample data at https://s3.amazonaws.com/datashader-data/nyc_taxi_wide.parq
+And put in this folder
--- embedding/deep/my_django_bokeh_site/djangobokeh/bokeh_apps/nyc_pickup.py
+++ embedding/deep/my_django_bokeh_site/djangobokeh/bokeh_apps/nyc_pickup.py
@@ -9,7 +9,7 @@ from geoviews.tile_sources import EsriImagery
 
 hv.extension('bokeh', 'matplotlib', width="100")
 
-# Download sample data for nyc_pickup.py at https://s3.amazonaws.com/datashader-data/nyc_taxi_wide.parq
+# Download sample data at https://s3.amazonaws.com/datashader-data/nyc_taxi_wide.parq
 # And put in folder "jupyter_to_tethys/embedding/deep/my_django_bokeh_site/djangobokeh/bokeh_apps/data"
 
 
